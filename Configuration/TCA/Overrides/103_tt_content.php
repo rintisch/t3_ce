dd2251cef2b7b0f6754ae63e5e43ce1411cdd022
rintisch <rintisch@stadtwerk.org>
2020-08-05 15:02:24 +0200

    Add 2col container

--- Configuration/TCA/Overrides/103_tt_content.php
+++ Configuration/TCA/Overrides/103_tt_content.php
@@ -14,4 +14,52 @@
     ],
     'EXT:t3_ce/Resources/Public/Icons/ContentElements/container-4col.svg'
 );
+$GLOBALS['TCA']['tt_content']['types']['t3ce-4cols-container']['showitem'] = '
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;general,
+        --palette--;;headers,
+    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;;frames,
+        --palette--;;appearanceLinks,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+        --palette--;;language,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;hidden,
+        --palette--;;access,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+        categories,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+        rowDescription,
+';
 
+## Add 2 col container
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->addContainer(
+    't3ce-2cols-container',
+    '2 Column Container',
+    'Container for 2 columns',
+    [
+        [
+            ['name' => 'left', 'colPos' => 201],
+            ['name' => 'right', 'colPos' => 202],
+        ]
+    ],
+    'EXT:t3_ce/Resources/Public/Icons/ContentElements/container-2col.svg'
+);
+$GLOBALS['TCA']['tt_content']['types']['t3ce-2cols-container']['showitem'] = '
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;general,
+        --palette--;;headers,
+    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;;frames,
+        --palette--;;appearanceLinks,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+        --palette--;;language,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;hidden,
+        --palette--;;access,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+        categories,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+        rowDescription,
+';

751e1e466e9a3babaed1629491fa7997d6cae15f
rintisch <rintisch@stadtwerk.org>
2022-03-23 14:36:17 +0100

    [task] Add CE grid container
    Content element which wraps the childs to
    display them as a grid.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -8,6 +8,6 @@ return [
     'content-t3ce-page-hero'  => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:t3_ce/Resources/Public/Icons/ContentElements/page-hero.svg'
-    ]
+    ],
 ];
 
